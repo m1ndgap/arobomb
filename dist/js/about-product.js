1417c9d46d6c81b29228e748346198086edc9c52
Tim <neusaneto@gmail.com>
2020-05-20 17:48:59 +0200

    best i could do

---
+++ dist/js/about-product.js
@@ -0,0 +1,12 @@
+"use strict"
+
+var aboutProductSwiper = new Swiper('.about-product__swiper-container', {
+    speed: 400,
+    pagination: {
+        el: '.about-product__swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+    }
+});
+
+
