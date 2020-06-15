cbda3c64a86eed4c68122aa13b52508464bb1877
Tim <neusaneto@gmail.com>
2020-06-15 13:25:29 +0200

    almost done with landing-1

--- dist/js/landing-1.js
+++ dist/js/landing-1.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var landingSwiper = new Swiper('.swiper-container', {
+let landing1TabsSwiper = new Swiper('.tabs-swiper-container', {
   speed: 400,
   slidesPerView: 5,
   spaceBetween: 30,
@@ -12,3 +12,12 @@ var landingSwiper = new Swiper('.swiper-container', {
     prevEl: '.swiper-button-prev',
   },
 });
+
+let landing1PartnersSwiper = new Swiper('.partners-swiper-container', {
+  speed: 400,
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+})
