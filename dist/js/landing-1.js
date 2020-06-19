129893546480e0981e3721f25cd8a3e98d76b4bd
Tim <neusaneto@gmail.com>
2020-06-19 19:36:00 +0200

    restarted responsive

--- dist/js/landing-1.js
+++ dist/js/landing-1.js
@@ -1,48 +1,54 @@
 "use strict";
 
 let landing1TabsSwiper = new Swiper('.tabs-swiper-container', {
-  speed: 400,
-  slidesPerView: 1,
-  spaceBetween: 0,
-  observer: true,
-  centeredSlides: true,
-  grabCursor: true,
-  loop: true,
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-  breakpoints: {
-    400: {
-      slidesPerView: 2,
-      spaceBetween: 20,
-      centeredSlides: false,
+	speed: 400,
+	slidesPerView: 1,
+	spaceBetween: 30,
+	observer: true,
+	centeredSlides: true,
+	grabCursor: true,
+	loop: true,
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+	pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
     },
-    500: {
-      slidesPerView: 3,
-    },
-
-    600: {
-      slidesPerView: 4,
-      centeredSlides: false,
-    },
-
-    900: {
-      slidesPerView: 5,
-      spaceBetween: 30
-    }
-  }
+	breakpoints: {
+		400: {
+			slidesPerView: 2,
+			spaceBetween: 20,
+			centeredSlides: false,
+		},
+		500: {
+			slidesPerView: 3,
+		},
+		
+		600: {
+			slidesPerView: 4,
+			centeredSlides: false,
+		},
+		
+		900: {
+			slidesPerView: 5,
+			spaceBetween: 30
+		}
+	}
 });
 
+
 let landing1PartnersSwiper = new Swiper('.partners-swiper-container', {
-  speed: 400,
-  spaceBetween: 30,
-  loop: true,
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+	speed: 400,
+	spaceBetween: 30,
+	loop: true,
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+	pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    }
 })
