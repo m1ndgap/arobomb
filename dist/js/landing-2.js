b0e06e87142b89e070a79ebf88a1c94598c3fa52
Tim Chumbash <posseslash@gmail.com>
2020-06-30 14:32:18 +0200

    calculator done

--- dist/js/landing-2.js
+++ dist/js/landing-2.js
@@ -11,8 +11,33 @@ function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
+// каждые 100к = 20 партнеров
+// партнеры  * 4к = вложения
+// продажи = партнеры * 25?
+// прибыль продажи * 60рублей?
+
+function updateCalculations(valueStr, obj) {
+    let {partnersEl, investEl, moSalesEl, profitEl} = obj
+    let value = +valueStr.replace(',', '')
+    let partners = value / 100000 * 20
+    let investment = partners * 4000
+    let sales = partners * 25
+    let profit = sales * 60
+    partnersEl.innerText = partners;
+    investEl.innerText = `${numberWithCommas(investment)} р.`
+    moSalesEl.innerText = sales;
+    profitEl.innerText = `${numberWithCommas(profit)} р.`
+}
+
 
 let currentValue = document.querySelector(`.${currentValueCls}`);
+let updateObj = {
+    partnersEl: document.querySelector(`#${partnersId}`),
+    investEl: document.querySelector(`#${investId}`),
+    moSalesEl: document.querySelector(`#${moSalesId}`),
+    profitEl: document.querySelector(`#${profitId}`),
+}
+
 var calcSlider = new rSlider({
     target: '#calculator-range',
     values: [`100,000`, `150,000`, `200,000`, `250,000`, `300,000`, `350,000`, `400,000`, `450,000`, `500,000`],
@@ -23,15 +48,9 @@ var calcSlider = new rSlider({
     set: [`200,000`],
     onChange: function(vals) {
         currentValue.innerText = vals
+        updateCalculations(vals, updateObj)
     }
 });
-
-console.log(calcSlider)
-calcSlider.getValue()
-calcSlider.onChange(function (values) {
-    console.log(123);
-});
-
 
 // calcSlider.addEventListener('change', function (evt) {
 //     console.log(1231)
