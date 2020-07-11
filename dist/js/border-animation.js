82a7d812a4622862211e251c5b4f9feb0155b3a6
Tim Chumbash <posseslash@gmail.com>
2020-07-12 00:29:12 +0200

    animation wip

--- dist/js/border-animation.js
+++ dist/js/border-animation.js
@@ -1,3 +1,29 @@
 "use strict";
 
-let
+const animateHost = document.getElementById(`animate-test-host`)
+const animation = animateHost.getElementById(`animate-test`)
+
+const animateCircle = document.getElementById(`test-circle`)
+const animationCircle = animateCircle.getElementById(`test-circle-to`)
+
+animateHost.addEventListener(`mouseenter`, function () {
+    animation.beginElementAt(0);
+    console.log(animation.getStartTime())
+    console.log(animation.getSimpleDuration())
+
+})
+animateHost.addEventListener(`mouseleave`, function () {
+    animation.endElement();
+    console.log(animation.getCurrentTime())
+})
+
+
+
+
+animateCircle.addEventListener(`mouseenter`, function () {
+    animationCircle.beginElement();
+
+})
+animateCircle.addEventListener(`mouseleave`, function () {
+    animationCircle.endElement();
+})
