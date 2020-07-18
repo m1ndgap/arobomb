fb8e6f88a7b68465f7eb8f61d4443450b16e60d8
Tim Chumbash <posseslash@gmail.com>
2020-07-18 22:51:37 +0200

    constructor wip

--- dist/js/constructor-index.js
+++ dist/js/constructor-index.js
@@ -1,2 +1,21 @@
 "use strict";
 
+let constructorBlobs = document.querySelectorAll(`.constructor-blob-svg`);
+
+constructorBlobs.forEach(function (blob) {
+    let parent = blob.parentElement;
+    let animate = blob.querySelector(`animate`)
+    parent.addEventListener(`mouseenter`, function(){
+        if (blob.animationsPaused()) {
+            console.log(123123)
+            blob.unpauseAnimations();
+        } else {
+            console.log(333333)
+            animate.beginElement();
+        }
+    })
+
+    parent.addEventListener(`mouseleave`, function(){
+        blob.pauseAnimations();
+    })
+})
