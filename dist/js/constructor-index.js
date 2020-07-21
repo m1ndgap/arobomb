973320aee3ce884e062f4556beb76067169185ea
Tim Chumbash <posseslash@gmail.com>
2020-07-21 16:49:16 +0200

    index box switching done

--- dist/js/constructor-index.js
+++ dist/js/constructor-index.js
@@ -1,5 +1,8 @@
 "use strict";
 
+const mobBtnCls = `constructor__box-mobile-selector-span`;
+const boxBlocks = ``
+
 let constructorBlobs = document.querySelectorAll(`.constructor-blob-svg`);
 
 constructorBlobs.forEach(function (blob) {
@@ -7,10 +10,8 @@ constructorBlobs.forEach(function (blob) {
     let animate = blob.querySelector(`animate`)
     parent.addEventListener(`mouseenter`, function(){
         if (blob.animationsPaused()) {
-            console.log(123123)
             blob.unpauseAnimations();
         } else {
-            console.log(333333)
             animate.beginElement();
         }
     })
@@ -18,4 +19,71 @@ constructorBlobs.forEach(function (blob) {
     parent.addEventListener(`mouseleave`, function(){
         blob.pauseAnimations();
     })
+})
+
+let mobileBtns = document.querySelectorAll(`.${mobBtnCls}`);
+let box1 = document.querySelector(`.js-box-1-mobile`);
+let box2 = document.querySelector(`.js-box-2-mobile`);
+
+
+mobileBtns.forEach(function(btn){
+    btn.addEventListener(`click`, function(evt){
+        evt.preventDefault();
+        let type = this.dataset.boxtype;
+
+        let isActive = this.classList.contains(`${mobBtnCls}--active`);
+        if (type == "small" && !isActive) {
+            box1.classList.add(`js-box-1-mobile-active`)
+            box1.classList.remove(`js-box-1-mobile-inactive`)
+            box2.classList.remove(`js-box-2-mobile-active`)
+            box2.classList.add(`js-box-2-mobile-inactive`)
+            currentBoxType = boxTypes.small;
+        } else if (type == "big" && !isActive) {
+            box2.classList.add(`js-box-2-mobile-active`)
+            box2.classList.remove(`js-box-2-mobile-inactive`)
+            box1.classList.remove(`js-box-1-mobile-active`)
+            box1.classList.add(`js-box-1-mobile-inactive`)
+            currentBoxType = boxTypes.big;
+        }
+
+        mobileBtns.forEach(function (mbBtn) {
+            mbBtn.classList.remove(`${mobBtnCls}--active`)
+        })
+        this.classList.add(`${mobBtnCls}--active`);
+
+    })
+})
+
+box1.addEventListener(`click`, function(){
+    if (box1.classList.contains(`js-box-1-mobile-inactive`)) {
+        box1.classList.add(`js-box-1-mobile-active`)
+        box1.classList.remove(`js-box-1-mobile-inactive`)
+        box2.classList.remove(`js-box-2-mobile-active`)
+        box2.classList.add(`js-box-2-mobile-inactive`)
+
+        mobileBtns.forEach(function (mbBtn) {
+            if (mbBtn.dataset.boxtype !== "small") {
+                mbBtn.classList.remove(`${mobBtnCls}--active`)
+            } else {
+                mbBtn.classList.add(`${mobBtnCls}--active`)
+            }
+        })
+    }
+})
+
+box2.addEventListener(`click`, function(){
+    if (box2.classList.contains(`js-box-2-mobile-inactive`)) {
+        box2.classList.add(`js-box-2-mobile-active`)
+        box2.classList.remove(`js-box-2-mobile-inactive`)
+        box1.classList.remove(`js-box-1-mobile-active`)
+        box1.classList.add(`js-box-1-mobile-inactive`)
+
+        mobileBtns.forEach(function (mbBtn) {
+            if (mbBtn.dataset.boxtype !== "big") {
+                mbBtn.classList.remove(`${mobBtnCls}--active`)
+            } else {
+                mbBtn.classList.add(`${mobBtnCls}--active`)
+            }
+        })
+    }
 })
--- dist/js/constructor-steps.js
+++ dist/js/constructor-steps.js
@@ -1,2 +1,35 @@
 "use strict";
 
+const blobBtnSmallCls = `constructor__blob-btn--1`;
+const blobBtnBigCls = `constructor__blob-btn--2`;
+
+
+const boxTypes = {
+    big: {
+        souvenirs: 6,
+        aromas: 12,
+        price: 3500
+    },
+    small: {
+        souvenirs: 2,
+        aromas: 6,
+        price: 1500
+    }
+}
+
+let currentBoxType = {};
+
+
+
+let blobBtnSmall = document.querySelector(`.${blobBtnSmallCls}`);
+let blobBtnBig = document.querySelector(`.${blobBtnBigCls}`);
+
+blobBtnSmall.addEventListener(`click`, function(evt){
+    evt.preventDefault();
+    currentBoxType = boxTypes.small;
+});
+
+blobBtnBig.addEventListener(`click`, function(evt){
+    evt.preventDefault();
+    currentBoxType = boxTypes.big;
+});
