18324d17d9f2e4f7e3771bef428c6c8ae7a9355e
Tim Chumbash <posseslash@gmail.com>
2020-08-13 13:51:08 +0200

    callback form tel validation

--- dist/js/callback-modal.js
+++ dist/js/callback-modal.js
@@ -5,15 +5,27 @@ const cbBtnCls = `js-order-call-btn`;
 
 const cbModalFormCls = `cbmodal-form__wrapper`;
 const cbModalThanksCls = `cbmodal-thanks__wrapper`;
+const cbModalPhone = `cbmodal-form__tel`;
 
 const cbmodalCloseCls = `modal__close`;
 const cbModalSubmitCls = `cbmodal-form__submit-btn`;
+
+function validatePhone(phoneEl) {
+    let phone = phoneEl.value,
+        numbers = /^\s*(?:\+?(\d{1,3}))?([-. (]*(\d{3})[-. )]*)?((\d{3})[-. ]*(\d{2,4})(?:[-.x ]*(\d+))?)\s*$/gm;
+    if(phone.match(numbers)) {
+        return phone
+    } else {
+        return false
+    }
+}
 
 
 let cbBtn = document.querySelector(`.` + cbBtnCls);
 let cbModal = document.querySelector(`.` + cbmodalCls);
 
 let cbSubmit = cbModal.querySelector(`.` + cbModalSubmitCls);
+let cbPhone = cbModal.querySelector(`#` + cbModalPhone);
 let cbClose = cbModal.querySelector(`.` + cbmodalCloseCls);
 let cbcbModalForm = cbModal.querySelector(`.` + cbModalFormCls);
 let cbModalThanks = cbModal.querySelector(`.` + cbModalThanksCls);
@@ -24,7 +36,13 @@ cbBtn.addEventListener(`click`, function(e){
 })
 
 cbSubmit.addEventListener(`click`, function (e) {
-    // e.preventDefault();
+    if (!validatePhone(cbPhone)) {
+        e.preventDefault();
+        cbPhone.classList.add(`cbmodal-form__input--error`)
+        cbPhone.value = ``
+        cbPhone.placeholder = `введите номер телефона`
+        console.log("error")
+    }
     // cbcbModalForm.classList.remove(`modal-wrapper--active`)
     // cbModalThanks.classList.add(`modal-wrapper--active`)
 })
