beb36886492de69e43b6410f7ba8b2e366699b5b
Tim Chumbash <posseslash@gmail.com>
2020-10-02 21:10:23 +0200

    constructor landing

---
+++ dist/js/constructor-landing.js
@@ -0,0 +1,33 @@
+let landing1TabsSwiper = new Swiper('.constructor-landing-swiper-container', {
+    speed: 400,
+    slidesPerView: 2,
+    spaceBetween: 45,
+    observer: true,
+    centeredSlides: false,
+    grabCursor: true,
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    breakpoints: {
+        500: {
+            slidesPerView: 3,
+            centeredSlides: true,
+        },
+
+        600: {
+            slidesPerView: 4,
+            centeredSlides: false,
+        },
+
+        900: {
+            slidesPerView: 5,
+            centeredSlides: true,
+        }
+    }
+});
