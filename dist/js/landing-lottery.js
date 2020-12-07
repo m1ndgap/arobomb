0e0bf59b0ca52c77b7d270a536e36db8f4986f4a
Tim Chumbash <posseslash@gmail.com>
2020-12-07 12:01:00 +0100

    lottery wip

---
+++ dist/js/landing-lottery.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const btnCls = `landing-lottery__btn`;
+const textCls = `js-clipboard-text`;
+
+let clipboardBtn = document.querySelector(`.${btnCls}`)
+let clipboardTextBlock = document.querySelector(`.${textCls}`)
+
+clipboardBtn.addEventListener(`click`, function (){
+    let text = clipboardTextBlock.innerText
+    navigator.clipboard.writeText(text)
+})
