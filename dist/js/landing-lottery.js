fb8ee99383758603cbc9a35e47ef9a87d93287cb
Tim Chumbash <posseslash@gmail.com>
2020-12-08 05:11:50 +0100

    lottery landing done

--- dist/js/landing-lottery.js
+++ dist/js/landing-lottery.js
@@ -2,11 +2,48 @@
 
 const btnCls = `landing-lottery__btn`;
 const textCls = `js-clipboard-text`;
+const participateBtnCls = `landing-lottery__participate-btn`;
 
-let clipboardBtn = document.querySelector(`.${btnCls}`)
-let clipboardTextBlock = document.querySelector(`.${textCls}`)
+let clipboardBtn = document.querySelector(`.${btnCls}`);
+let clipboardTextBlock = document.querySelector(`.${textCls}`);
+let instaBtn = document.querySelector(`.${participateBtnCls}`);
 
-clipboardBtn.addEventListener(`click`, function (){
-    let text = clipboardTextBlock.innerText
-    navigator.clipboard.writeText(text).then(function(){}, function(){});
-})
+function getMobileOperatingSystem() {
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+    // Windows Phone must come first because its UA also contains "Android"
+    if (/windows phone/i.test(userAgent)) {
+        return "Windows Phone";
+    }
+
+    if (/android/i.test(userAgent)) {
+        return "Android";
+    }
+
+    // iOS detection from: http://stackoverflow.com/a/9039885/177710
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+        return "iOS";
+    }
+    return "unknown";
+}
+
+
+// clipboardBtn.addEventListener(`click`, function (){
+//     let text = clipboardTextBlock.innerText
+//     navigator.clipboard.writeText(text).then(function(){}, function(){});
+// })
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    switch (getMobileOperatingSystem()) {
+        case 'iOS':
+            instaBtn.setAttribute('href', instaBtn.dataset.ioslink)
+            break;
+        case 'Android':
+            instaBtn.setAttribute('href', instaBtn.dataset.androidlink)
+            break;
+        default:
+            break;
+    }
+});
+
+new ClipboardJS('.landing-lottery__btn');
